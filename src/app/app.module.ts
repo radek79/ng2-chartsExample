200301c4e03babc3c1a319b64fd9fdcff8b2e531
rpatulski <rod.patulski@onpeak.com>
2016-11-06 14:43:44 -0800

    successfully imported {ChartsModule} and put it in the imports statement of ngModel in order to try to use ng2-charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import {ChartsModule} from 'ng2-charts/components/charts/charts';
+
+console.log('the charts module = ', ChartsModule);
 
 @NgModule({
   declarations: [
@@ -15,7 +18,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    ChartsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
